732644608343eb790be0a12479fee1f6ca496dc6
Syed Aabis Abbas Zaidi <71585201+AabisZ@users.noreply.github.com>
2022-08-10 20:10:10 +0500

    import logo from './logo.svg';
    revert changes from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World Freom Aabis
+          Hello World From Aabis
         </p>
         <a
           className="App-link"
